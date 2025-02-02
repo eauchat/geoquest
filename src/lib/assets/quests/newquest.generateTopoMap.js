4b9f7ec0795f2f133ed946bf57c23ab3bea15e6d
squeak <squeak@eauchat.org>
2025-02-02 02:47:44 +0100

    Added newquest script to genereate topojson map from multiple geojson maps.

---
+++ src/lib/assets/quests/newquest.generateTopoMap.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+import _ from "lodash";
+import * as fs from "fs";
+import * as topojson from "topojson-server";
+
+import { abort, json_stringify_pretty,log } from "./newquest.utils.js";
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//                                                  GET ALL QUEST ELEMENTS
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+// figure out quest to autobuild
+const questId = process.argv[2];
+if (!questId) abort("Please pass new quest id as first argument!");
+
+// get list of quests
+let quests;
+try { quests = JSON.parse(fs.readFileSync(`./index.json`, { encoding: "utf8" })); }
+catch (err) { abort(`Failed to parse quests "index.json" file.`, err) };
+
+// check there is an object in quests to autobuild
+let questObject = _.find(quests, { id: questId });
+if (!questObject) abort(`You should create your new quest settings in "quests/index.json" file!`);
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//                                                  CREATE TOPO JSON FILE
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+// IMPORT BASEMAPS AND ELEMENTS MAPS
+let maps = _.map(questObject.maps, function (mapObject, index) {
+    let mapFile = JSON.parse(fs.readFileSync(`./${questId}/${mapObject.filePathRelativeToQuestDir}`))
+    _.each(mapFile.features, function (feature) {
+        if (!feature.properties[mapObject.namePropertyKey]) log.error("Could not find name for feature with properties:", feature.properties);
+        feature.properties = {
+            name: (mapObject.type == "basemap" ? " " : "") + feature.properties[mapObject.namePropertyKey] + (mapObject.type == "basemap" ? " " : ""), // adding spaces around name of basemaps elements is just a trick to avoid having collisions between elements and basemap pieces having the same name (for example Luxembourg capital being same as Luxembourg country)
+            type: mapObject.type,
+        };
+    });
+    return mapFile;
+});
+
+// MAKE A SINGLE SIMPLE TOPO FILE FROM MAPS
+let topo = topojson.topology(maps);
+let allGeometries = [];
+_.each(topo.objects, function (object) {
+  _.each(object.geometries, function (geometry) {
+    allGeometries.push(geometry);
+  });
+});
+topo.objects = {}
+topo.objects[questObject.objectsKey] = {
+    type: "GeometryCollection",
+    geometries: allGeometries,
+};
+
+fs.writeFileSync(`./${questId}/map.json`, JSON.stringify(topo));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
