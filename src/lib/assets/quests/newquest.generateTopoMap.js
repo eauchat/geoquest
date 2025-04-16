675442116f7a39485812adbeb3d5ea7939ca5f46
squeak <squeak@eauchat.org>
2025-04-16 13:36:53 +0200

    Pass all properties in quest index file automatically to elements. Necessary for island helper margins, also an improvement for styles, map type, and other properties previously passed one by one.

--- src/lib/assets/quests/newquest.generateTopoMap.js
+++ src/lib/assets/quests/newquest.generateTopoMap.js
@@ -27,13 +27,17 @@ let allTags = [];
 let maps = _.map(questObject.maps, function (mapObject, index) {
     let mapFile = JSON.parse(fs.readFileSync(`./${questId}/${mapObject.filePathRelativeToQuestDir}`))
     _.each(mapFile.features, function (feature) {
+
         var featureProperties = feature.properties;
         if (!featureProperties[mapObject.namePropertyKey]) log.error("Could not find name for feature with properties:", featureProperties);
-        feature.properties = {
-            name: (mapObject.type == "basemap" ? " " : "") + featureProperties[mapObject.namePropertyKey] + (mapObject.type == "basemap" ? " " : ""), // adding spaces around name of basemaps elements is just a trick to avoid having collisions between elements and basemap pieces having the same name (for example Luxembourg capital being same as Luxembourg country)
-            style: mapObject.style,
-            type: mapObject.type,
-        };
+        feature.properties = {};
+
+        _.each(mapObject, function (value, key) {
+            if (key !== "filePathRelativeToQuestDir") feature.properties[key] = value;
+        });
+
+        feature.properties.name = (mapObject.type == "basemap" ? " " : "") + featureProperties[mapObject.namePropertyKey] + (mapObject.type == "basemap" ? " " : ""); // adding spaces around name of basemaps elements is just a trick to avoid having collisions between elements and basemap pieces having the same name (for example Luxembourg capital being same as Luxembourg country)
+
         if (mapObject.type !== "basemap") {
             _.each(["gq_color", "gq_tags", "gq_info", "gq_helper"], function (key) {
                 if (!_.isUndefined(featureProperties[key])) feature.properties[key.replace(/^gq_/, "")] = featureProperties[key];
@@ -42,6 +46,7 @@ let maps = _.map(questObject.maps, function (mapObject, index) {
               allTags.push(tag);
             });
         };
+
     });
     return mapFile;
 });
